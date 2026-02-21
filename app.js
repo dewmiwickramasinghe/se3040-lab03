bb7ef72516e2ec31da6d0f723e717ba41f01135e
Dewmi <dewmiwickramasinghe55@gmail.com>
2026-02-21 20:13:09 +0530

    added write file features

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 console.log("Hello SE3040 Lab 03!");
 const fs = require('fs');
 
+// Read file
 fs.readFile('file.txt', 'utf8', function (err, data) {
     if (err) throw err;
     console.log(data);
+});
+// Write file
+fs.writeFile('file.txt', 'Hello World!', function (err) {
+    if (err) throw err;
+    console.log('File saved!');
 });
